7728ce594d7663d09f2d809e69ff20ceee287800
= <abhisingh30800@gmail.com>
2023-12-24 21:39:02 +0530

    added styling in recipe comp

--- Components/RecipeComp.tsx
+++ Components/RecipeComp.tsx
@@ -1,16 +1,29 @@
-import styles from '@/app/page.module.css';
-import Image from 'next/image';
-
-
+import styles from "@/app/page.module.css";
+import Image from "next/image";
 
 export default function RecipeComp({ item }: any) {
-    const { recipe: { images, label, summary, calories } } = item;
-    return (
-        <div className={styles.recipeComp}>
-            <Image loading='lazy' width={100} height={100} src={images?.REGULAR?.url} alt='' />
-            <h2>{label}</h2>
-            <p>{summary}</p>
-            <span>{calories.toFixed(2)}</span>
-        </div>
-    )
+  const {
+    recipe: { images, label, summary, calories },
+  } = item;
+  //   console.log(item);
+  return (
+    <div className={styles.recipeComp}>
+      <Image
+        style={{ width: "100%", height: "200px", objectFit: "cover" }}
+        priority={true}
+        width={100}
+        height={100}
+        src={images?.LARGE?.url || images?.REGULAR?.url}
+        alt={label}
+      />
+      <h2>{label}</h2>
+      <p>
+        {summary ? summary.substring(0, 70) + "..." : "No Summary Available"}
+      </p>
+      <p className={styles.calories}>
+        Calories
+        <span>{calories.toFixed(2)}</span>
+      </p>
+    </div>
+  );
 }
