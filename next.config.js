155f35a2e6751ab569f438b777f8a2174e2f1dc6
dazzlerabhi30800 <abhisingh30800@gmail.com>
2023-12-25 22:57:44 +0530

    fixed next config file

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["edamam-product-images.s3.amazonaws.com"],
-    },
-    experimental: { esmExternals: true }
-}
+  images: {
+    // domains: ["edamam-product-images.s3.amazonaws.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  experimental: { esmExternals: true },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
