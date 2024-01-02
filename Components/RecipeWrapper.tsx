79838220fb8d74f9cc20bab41bc793b9d70fbbd0
= <abhisingh30800@gmail.com>
2024-01-02 11:28:21 +0530

    added functionality to load more

--- Components/RecipeWrapper.tsx
+++ Components/RecipeWrapper.tsx
@@ -1,7 +1,12 @@
 "use client";
 import styles from "@/app/page.module.css";
-import { Suspense, useEffect } from "react";
-import { useAppSelector, fetchRecipes, AppDispatch } from "@/Store/Slice";
+import { useEffect } from "react";
+import {
+  useAppSelector,
+  fetchRecipes,
+  AppDispatch,
+  nextPageRecipes,
+} from "@/Store/Slice";
 import { useDispatch } from "react-redux";
 import RecipeComp from "./RecipeComp";
 import Loading from "@/app/loading";
@@ -9,6 +14,7 @@ import Loading from "@/app/loading";
 export default function Recipes() {
   const loading = useAppSelector((state) => state.auth.loading);
   const recipes = useAppSelector((state) => state.auth.recipes);
+  const nextPageLink = useAppSelector((state) => state.auth.nextPageLink);
   const dispatch = useDispatch<AppDispatch>();
   useEffect(() => {
     if (recipes.length >= 1) return;
@@ -17,10 +23,18 @@ export default function Recipes() {
   const recipeData = [].concat(...recipes?.map((item: any) => item.hits));
   if (loading) return <Loading />;
   return (
-    <div className={styles.recipeWrapper}>
-      {recipeData.map((item: any, index) => (
-        <RecipeComp key={index} item={item} />
-      ))}
-    </div>
+    <>
+      <div className={styles.recipeWrapper}>
+        {recipeData.map((item: any, index) => (
+          <RecipeComp key={index} item={item} />
+        ))}
+      </div>
+      <button
+        onClick={() => dispatch(nextPageRecipes(nextPageLink))}
+        className={`${styles.button} ${styles.nextButton}`}
+      >
+        Load More Recipes
+      </button>
+    </>
   );
 }
--- Store/Slice.tsx
+++ Store/Slice.tsx
@@ -11,11 +11,20 @@ const appKey = process.env.NEXT_PUBLIC_APP_KEY?.toString();
 
 export const fetchRecipes = createAsyncThunk("auth/fetchRecipes", async () => {
   const data = await fetch(
-    `https://api.edamam.com/api/recipes/v2?q=paneer&app_key=${appKey}&app_id=${appId}&type=public`
+    `https://api.edamam.com/api/recipes/v2?q=paneer&app_key=${appKey}&app_id=${appId}&type=public`,
   );
   const response = await data.json();
   return response;
 });
+
+export const nextPageRecipes = createAsyncThunk<any, any, any>(
+  "auth/nextPageRecipes",
+  async (url: string) => {
+    const data = await fetch(url);
+    const response = await data.json();
+    return response;
+  },
+);
 
 export type fetchRecipeByString = {
   searchString: string;
@@ -25,23 +34,25 @@ export const searchRecipes = createAsyncThunk<any, any, any>(
   "auth/searchRecipes",
   async (searchString: any) => {
     const data = await fetch(
-      `https://api.edamam.com/api/recipes/v2?q=${searchString}&app_key=${appKey}&app_id=${appId}&type=public`
+      `https://api.edamam.com/api/recipes/v2?q=${searchString}&app_key=${appKey}&app_id=${appId}&type=public`,
     );
     const response = await data.json();
     return response;
-  }
+  },
 );
 
 export interface AuthState {
   loading: boolean;
   recipes: any;
   searchString: string;
+  nextPageLink: string | null;
 }
 
 const initialState = {
   loading: false,
   recipes: [],
   searchString: "",
+  nextPageLink: null,
 } as AuthState;
 
 export const authSlice = createSlice({
@@ -54,6 +65,9 @@ export const authSlice = createSlice({
     setSearchString(state, action) {
       state.searchString = action.payload;
     },
+    setNextPage(state, action) {
+      state.nextPageLink = action.payload;
+    },
   },
   // Special reducers for hydrating the state. Special case for next-redux-wrapper
   extraReducers: (builder) => {
@@ -62,6 +76,15 @@ export const authSlice = createSlice({
     });
     builder.addCase(fetchRecipes.fulfilled, (state, action) => {
       state.recipes.push(action.payload);
+      state.nextPageLink = action.payload._links.next.href;
+      state.loading = false;
+    });
+    builder.addCase(nextPageRecipes.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(nextPageRecipes.fulfilled, (state, action) => {
+      state.recipes.push(action.payload);
+      state.nextPageLink = action.payload._links.next.href;
       state.loading = false;
     });
     builder.addCase(searchRecipes.pending, (state) => {
@@ -80,7 +103,8 @@ export const authSlice = createSlice({
   },
 });
 
-export const { setLoadingState, setSearchString } = authSlice.actions;
+export const { setLoadingState, setSearchString, setNextPage } =
+  authSlice.actions;
 
 export const store = configureStore({
   reducer: {
